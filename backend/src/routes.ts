c3706d1f306e015dc74500c67bc0a0536958e02a
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2023-01-07 18:38:22 -0300

    Cria rota, controller e service do usuario

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,16 @@
-import { Router, Request, Response } from 'express';
+// import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({iTherapist: "Online!"})
-    //throw new Error('Erro ao fazer essa requisição.')
-});
+// router.get('/teste', (req: Request, res: Response) => {
+//     return res.json({iTherapist: "Online!"})
+//     //throw new Error('Erro ao fazer essa requisição.')
+// });
+
+//--Rotas Users --
+router.post('/users', new CreateUserController().handle)
 
 export { router };
