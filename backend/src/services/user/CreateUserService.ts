76b91ead952520fe56c768c743ce24525d2cebfb
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2023-01-07 20:20:05 -0300

    Cadastra Usuario no banco

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prismaClient from '../../prisma'; // Importa direto, foi exportado como default
 
 interface UserRequest{
     name: string;
@@ -7,9 +8,39 @@ interface UserRequest{
 
 class CreateUserService{
    async execute({name, email, password}: UserRequest){
-    console.log(name);
+    // Verificar se ele enviou um email
+    if(!email){
+        throw new Error("Email incorect.")
+    }
+
+    // Verificar se esse email já está cadastrado na plataforma
+    const userAreadyExists = await prismaClient.user.findFirst({
+        where: {
+            email: email
+        }
+    })
+
+    if(userAreadyExists){
+        throw new Error("User already exist.");
+    }
+
+    const user = await prismaClient.user.create({
+        data:{
+           name: name,
+           email: email,
+           password: password, 
+        },
+        select:{
+            id: true,
+            name: true,
+            email: true,
+        }
+    })
+    
+    // console.log(name);    
     // return { Ok: true }
-    return { name: name }
+    // return { name: name }
+    return user;
    } 
 }
 
